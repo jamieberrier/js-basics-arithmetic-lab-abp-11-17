b086998dd245a8e88807e66ae95b705d15f1a7e4
jamieberrier <berrier.jamie@gmail.com>
2017-11-15 19:58:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,5 @@ const ageIsValid = Number.isInteger(currentAge);
 const randomNumber = Math.floor(Math.random()*20)+1;
 
 const randomInteger = Math.round(randomNumber);
+
+const randomUserID = randomInteger + 1000000000;
